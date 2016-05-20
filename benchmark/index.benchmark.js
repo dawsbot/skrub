96b2e1520008dd5a551cba53a3ed27a55862f0ea
Dawson Botsford <DawsonBotsford@gmail.com>
2016-05-20 03:17:05 -0600

    fix broken lint in benchmark code

--- benchmark/index.benchmark.js
+++ benchmark/index.benchmark.js
@@ -1,36 +1,38 @@
 'use strict';
+const fs = require('fs');
 const Benchmark = require('benchmark');
-let suite = new Benchmark.Suite;
+let suite = new Benchmark.Suite();
 require('babel-polyfill');
 
+const tempFile = require('tempfile');
 const skrub = require('../index.js');
-const tempfile = require('tempfile');
-const fs = require('fs');
 
 function _writeTempFile(data) {
-	const fileName = tempFile.sync();
-	fs.writeFileSync(data, fileName);
-	return fileName;
+  const fileName = tempFile.sync();
+  fs.writeFileSync(data, fileName);
+  return fileName;
 }
 
 const myData = 'the quick brown fox jumped over the lazy dog';
 
 // add tests
 suite.add('skrub(tempFile, {iterations: 1})', async function() {
-	await skrub(_writeTempFile(myData, {iterations: 1}));
+  await skrub(_writeTempFile(myData, {iterations: 1}));
 })
 .add('skrub(tempFile, {iterations: 7})', async function() {
-	await skrub(_writeTempFile(myData, {iterations: 7}));
+  await skrub(_writeTempFile(myData, {iterations: 7}));
 })
 .add('skrub(tempFile, {iterations: 36})', async function() {
-	await skrub(_writeTempFile(myData, {iterations: 36}));
+  await skrub(_writeTempFile(myData, {iterations: 36}));
 })
 // add listeners
-.on('cycle', function(event) {
+.on('cycle', function (event) {
   console.log(String(event.target));
 })
-.on('complete', function() {
+.on('complete', function () {
   console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 // run async
-.run({ 'async': true });
+.run({
+  async: true
+});
